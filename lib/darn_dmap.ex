a17992ff78f05ef14a61c6a2bab3a6a8ceace454
Matt Caldwell <StoatPower@users.noreply.github.com>
2026-06-14 21:34:12 -0400

    refactor read api towards more idiomatic elixir

--- lib/darn_dmap.ex
+++ lib/darn_dmap.ex
@@ -6,14 +6,6 @@ defmodule DarnDmap do
   alias DarnDmap.Read
 
   defdelegate read(path, ftype, opts \\ []), to: Read
-  defdelegate read_lax(path, ftype, opts \\ []), to: Read
-  defdelegate read_bytes(bytes, ftype, opts \\ []), to: Read
-  defdelegate read_bytes_lax(bytes, ftype, opts \\ []), to: Read
-  defdelegate read_by_indices(path, ftype, indices, opts \\ []), to: Read
-  defdelegate read_by_indices_lax(path, ftype, indices, opts \\ []), to: Read
-  defdelegate read_bytes_by_indices(bytes, ftype, indices, opts \\ []), to: Read
-  defdelegate read_bytes_by_indices_lax(bytes, ftype, indices, opts \\ []), to: Read
   defdelegate read_metadata(path, ftype, opts \\ []), to: Read
-  defdelegate read_metadata_by_indices(path, ftype, indices, opts \\ []), to: Read
 
 end
--- lib/darn_dmap/read.ex
+++ lib/darn_dmap/read.ex
@@ -1,71 +1,71 @@
 defmodule DarnDmap.Read do
   alias DarnDmap.{Decode, Native}
 
-  def read(path, ftype, opts \\ []) do
-    path
-    |> Native.read(ftype)
+  def read(_source, _ftype, opts \\ [])
+
+  def read({:path, path}, ftype, opts), do: read(path, ftype, opts)
+
+  def read({:bytes, bytes}, ftype, opts) do
+    lax? = Keyword.get(opts, :lax?, false)
+    indices = Keyword.get(opts, :indices, [])
+
+    case {lax?, indices} do
+      {false, []} ->
+        Native.read_bytes(bytes, ftype)
+
+      {true, []} ->
+        Native.read_bytes_lax(bytes, ftype)
+
+      {false, idxs} when is_list(idxs) ->
+        Native.read_bytes_by_indices(bytes, ftype, idxs)
+
+      {true, idxs} when is_list(idxs) ->
+        Native.read_bytes_by_indices_lax(bytes, ftype, idxs)
+    end
     |> maybe_decode(opts)
   end
 
-  def read_lax(path, ftype, opts \\ []) do
-    path
-    |> Native.read_lax(ftype)
+  def read(path, ftype, opts) do
+    lax? = Keyword.get(opts, :lax?, false)
+    indices = Keyword.get(opts, :indices, [])
+
+    case {lax?, indices} do
+      {false, []} ->
+        Native.read(path, ftype)
+
+      {true, []} ->
+        Native.read_lax(path, ftype)
+
+      {false, idxs} when is_list(idxs) ->
+        Native.read_by_indices(path, ftype, idxs)
+
+      {true, idxs} when is_list(idxs) ->
+        Native.read_by_indices_lax(path, ftype, idxs)
+    end
     |> maybe_decode(opts)
   end
 
-  def read_bytes(bytes, ftype, opts \\ []) do
-    bytes
-    |> Native.read_bytes(ftype)
+  def read_metadata(path, ftype, opts \\ [decode?: true]) do
+    indices = Keyword.get(opts, :indices, [])
+    case indices do
+      [] ->
+        Native.read_metadata(path, ftype)
+
+      idxs when is_list(idxs) ->
+        Native.read_metadata_by_indices(path, ftype, idxs)
+    end
     |> maybe_decode(opts)
   end
 
-  def read_bytes_lax(bytes, ftype, opts \\ []) do
-    bytes
-    |> Native.read_bytes_lax(ftype)
-    |> maybe_decode(opts)
+  defp maybe_decode({records, bad_bytes}, opts) do
+    {maybe_decode(records, opts), bad_bytes}
   end
 
-  def read_by_indices(path, ftype, indices, opts \\ []) do
-    path
-    |> Native.read_by_indices(ftype, indices)
-    |> maybe_decode(opts)
-  end
-
-  def read_by_indices_lax(path, ftype, indices, opts \\ []) do
-    path
-    |> Native.read_by_indices_lax(ftype, indices)
-    |> maybe_decode(opts)
-  end
-
-  def read_bytes_by_indices(bytes, ftype, indices, opts \\ []) do
-    bytes
-    |> Native.read_bytes_by_indices(ftype, indices)
-    |> maybe_decode(opts)
-  end
-
-  def read_bytes_by_indices_lax(bytes, ftype, indices, opts \\ []) do
-    bytes
-    |> Native.read_bytes_by_indices_lax(ftype, indices)
-    |> maybe_decode(opts)
-  end
-
-  def read_metadata(path, ftype, opts \\ [decode: false]) do
-    path
-    |> Native.read_metadata(ftype)
-    |> maybe_decode(opts)
-  end
-
-  def read_metadata_by_indices(path, ftype, indices, opts \\ [decode: false]) do
-    path
-    |> Native.read_metadata_by_indices(ftype, indices)
-    |> maybe_decode(opts)
-  end
-
-  defp maybe_decode(records, opts) do
-    if Keyword.get(opts, :decode, false) do
-      records
-    else
+  defp maybe_decode(records, opts) when is_list(records) do
+    if Keyword.get(opts, :decode?, true) do
       Decode.decode_records(records)
+    else
+      records
     end
   end
 end
